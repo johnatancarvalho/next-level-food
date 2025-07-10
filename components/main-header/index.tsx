73634decab049d809cb42cc080a7992b2acab5ae
Johnatan Carvalho <johnatancarvalho06@gmail.com>
2025-07-10 16:26:18 -0300

    chore: improvements to main header

--- components/main-header/index.tsx
+++ components/main-header/index.tsx
@@ -1,28 +1,33 @@
 import Link from "next/link";
 import Image from "next/image";
 
+import HeaderBackground from "./header-background";
+
 import logoImg from "@/assets/logo.png";
 
 import css from "./index.module.css";
 
 export default function MainHeader() {
   return (
-    <header className={css.header}>
-      <Link className={css.logo} href="/">
-        <Image src={logoImg} alt="A plate with food on it" priority />
-        NextLevel Food
-      </Link>
+    <>
+      <HeaderBackground />
+      <header className={css.header}>
+        <Link className={css.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
 
-      <nav className={css.nav}>
-        <ul className="flex gap-4">
-          <li>
-            <Link href="/meals">Browse Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <nav className={css.nav}>
+          <ul className="flex gap-4">
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
