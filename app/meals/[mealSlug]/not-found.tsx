de04860295fd7ef8aff6feb085341dc677e37902
Johnatan Carvalho <johnatancarvalho06@gmail.com>
2025-07-21 15:47:15 -0300

    chore: add not-found page to meal details

---
+++ app/meals/[mealSlug]/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="not-found">
+      <h1>Meal Not Found</h1>
+      <p>Unfortunately, we could not find the meal you were looking for.</p>
+    </div>
+  );
+}
