095e2bdad9871498a05337243df3bec864cad021
Johnatan Carvalho <johnatancarvalho06@gmail.com>
2025-07-21 15:47:04 -0300

    chore: implement meal details page

--- app/meals/[mealSlug]/page.tsx
+++ app/meals/[mealSlug]/page.tsx
@@ -1,14 +1,54 @@
 import React from "react";
 
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+import { getMeal } from "@/utils/data/meals";
+
+import css from "./page.module.css";
+
 interface Props {
   params: Promise<{
     mealSlug: string;
   }>;
 }
 
-const MealPage = async ({ params }: Props) => {
+const MealDetailsPage = async ({ params }: Props) => {
   const { mealSlug } = await params;
-  return <h1>Meal: {mealSlug}</h1>;
+
+  const meal = await getMeal(mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  const mealInstructions = meal.instructions.replace(/\n/g, "<br />");
+
+  return (
+    <>
+      <header className={css.header}>
+        <div className={css.image}>
+          <Image fill src={meal.image} alt="Meal Image" />
+        </div>
+        <div className={css.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={css.creator}>
+            by{" "}
+            <>
+              <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+            </>
+          </p>
+          <p className={css.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={css.instructions}
+          dangerouslySetInnerHTML={{ __html: mealInstructions }}
+        ></p>
+      </main>
+    </>
+  );
 };
 
-export default MealPage;
+export default MealDetailsPage;
--- utils/data/meals.ts
+++ utils/data/meals.ts
@@ -12,3 +12,21 @@ export const getMeals = async () => {
   }
   return meals as Meal[];
 };
+
+export const getMeal = async (mealSlug: string) => {
+  const supabase = await createClient();
+
+  const { data: meal, error } = await supabase
+    .from("meals")
+    .select()
+    .eq("slug", mealSlug)
+    .maybeSingle();
+
+  if (error) {
+    throw new Error(`Failed to fetch meal: ${error?.message}`);
+  }
+  if(!meal) {
+    return undefined;
+  }
+  return meal as Meal;
+};
